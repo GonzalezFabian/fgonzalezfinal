b66aa022ec350a31a249f173e2f8c29135ddd538
Infografia1 <infografia1@Mac-Studio-de-Infografia1.local>
2023-05-16 17:49:55 -0600

    final

---
+++ src/componets/Banner/Banner.js
@@ -0,0 +1,15 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className='banner'>
+      <div className='banner-container'>
+        <h1>BCR
+        
+        </h1>
+        <p>VIVE - DISFRUTA - CORRE</p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
